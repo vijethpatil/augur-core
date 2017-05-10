36465574ebfb278d5e85c72501da3f4e2cff905c
Jack Peterson <jack@tinybike.net>
2017-05-10 14:54:56 -0700

    Moved update private chain script to UI repo

--- load_contracts/update_private_chain_augur.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ssh jack@45.33.62.72 -t "cd /home/jack/augur && /home/jack/augur/update-local.sh && cp /home/jack/augur/src/env-9000.json /home/jack/augur/build/config/env.json && sudo service augur restart"
